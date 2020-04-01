6cc4d5189ddb7e6b63fb48c45b78fe03b06cd118
new-byt <tnewby@salisbury6c.ac.uk>
2020-04-01 19:26:17 +0100

    yes

---
+++ resources/js/firebase.js
@@ -0,0 +1,13 @@
+(function() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyD11Dy2-FX_S7TVSJE9tb8v9ER48J76wlQ",
+    authDomain: "auth.craigchambers.tk",
+    databaseURL: "https://canon-d4c8a.firebaseio.com",
+    projectId: "canon-d4c8a",
+    storageBucket: "canon-d4c8a.appspot.com",
+    messagingSenderId: "732286613264",
+    appId: "1:732286613264:web:daab8dc9e7319ab482d795",
+    measurementId: "G-HJJ9HKKC9Y"
+  };
+  firebase.initializeApp(firebaseConfig);
+})();
---
+++ resources/js/main.js
@@ -0,0 +1,72 @@
+var optionBox = document.getElementById("optionBox");
+var loginBox = document.getElementById("loginBox");
+var signupBox = document.getElementById("signupBox");
+var register = document.querySelector(".signup");
+var login = document.querySelector(".login");
+var signout = document.querySelector(".signout");
+
+function hider(option) {
+  optionBox.style.visibility = "hidden";
+  if (option == "login") {
+    loginBox.style.visibility = "visible";
+    signupBox.style.visibility = "hidden";
+  } else {
+    loginBox.style.visibility = "hidden";
+    signupBox.style.visibility = "visible";
+  }
+}
+
+/*Register*/
+register.addEventListener("submit", e => {
+  e.preventDefault();
+  const email = register.email.value;
+  const password = register.password.value;
+  const username = register.username.value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password);
+
+  async function checkUser() {
+    await firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        user.updateProfile({
+          displayName: username
+        });
+      }
+    });
+  }
+  checkUser();
+});
+/*Login*/
+login.addEventListener("submit", e => {
+  e.preventDefault();
+  const email = login.email.value;
+  const password = login.password.value;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(user => {
+      login.reset();
+      login.querySelector(".error").textContent = "Success";
+    })
+    .catch(e => {
+      login.querySelector(".error").textContent = e.message;
+    });
+});
+
+/*User state*/
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    login.classList.add("hidden");
+    register.classList.add("hidden");
+    signout.classList.remove("hidden");
+  } else {
+    login.classList.remove("hidden");
+    register.classList.remove("hidden");
+    signout.classList.add("hidden");
+  }
+});
+
+/*Sign out*/
+function signOut() {
+  firebase.auth().signOut();
+}
